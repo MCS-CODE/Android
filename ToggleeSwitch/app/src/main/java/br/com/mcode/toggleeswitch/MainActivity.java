40b9de072cc34fdd21242056723b2eda6ab4c52a
mcsdevbr <m.c.s@msn.com>
2022-01-15 20:38:54 -0300

    Toogle and switch ookk

--- ToggleeSwitch/app/src/main/java/br/com/mcode/toggleeswitch/MainActivity.java
+++ ToggleeSwitch/app/src/main/java/br/com/mcode/toggleeswitch/MainActivity.java
@@ -3,6 +3,8 @@ package br.com.mcode.toggleeswitch;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private Switch switchSenha;
     private ToggleButton toggleButtonSenha;
     private TextView textResultado;
+    private CheckBox lembrtarSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +24,30 @@ public class MainActivity extends AppCompatActivity {
         switchSenha = findViewById(R.id.switchSenha);
         toggleButtonSenha = findViewById(R.id.toggleButtonSenha);
         textResultado = findViewById(R.id.textResultado);
+        lembrtarSenha = findViewById(R.id.checkBoxSenha);
+
+        adicionarListener();
     }
 
     public void enviar(View view){
 
         if (switchSenha.isChecked()){
-            textResultado.setText("switch ligado");
+            textResultado.setText("btn_enviar_ligado");
         }else {
-            textResultado.setText("switch desligado");
+            textResultado.setText("btn_enviar_desligado");
         }
+    }
+
+    public void adicionarListener(){
+        lembrtarSenha.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (lembrtarSenha.isChecked()){
+                    textResultado.setText("ligado");
+                }else {
+                    textResultado.setText("desligado");
+                }
+            }
+        });
     }
 }
