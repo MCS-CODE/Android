ec4fb9a378fbe077f409e639f7494f015bf77abe
mcsdevbr <m.c.s@msn.com>
2022-01-29 17:06:59 -0300

    CardView Layouts

--- CardView/app/src/main/java/br/com/mcode/cardview/activity/MainActivity.java
+++ CardView/app/src/main/java/br/com/mcode/cardview/activity/MainActivity.java
@@ -2,15 +2,21 @@ package br.com.mcode.cardview.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.mcode.cardview.R;
 import br.com.mcode.cardview.activity.adapter.AdapterPostagem;
+import br.com.mcode.cardview.model.Postagem;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerPostagem;
+    private List<Postagem> listpostagens = new ArrayList<>();
 
 
     @Override
@@ -21,11 +27,36 @@ public class MainActivity extends AppCompatActivity {
         recyclerPostagem = findViewById(R.id.recyclerPostagem);
 
         //Definir um layout
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        //RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        /*
+        //Mudando a orientação do layout para o lado
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+
+         */
+        //GridLayout
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
         recyclerPostagem.setLayoutManager(layoutManager);
 
         //Definir um adapter
-        AdapterPostagem adapter = new AdapterPostagem();
+        this.preparaPostagem();
+        AdapterPostagem adapter = new AdapterPostagem(listpostagens);
         recyclerPostagem.setAdapter(adapter);
+
+    }
+
+    public void preparaPostagem(){
+        Postagem postagem = new Postagem("Marcio Cruz", "#Canada", R.drawable.imagem1);
+        this.listpostagens.add(postagem);
+
+        postagem = new Postagem("Adilma", "#Instambu", R.drawable.imagem2);
+        this.listpostagens.add(postagem);
+
+        postagem = new Postagem("Johnes Santana", "#Paris", R.drawable.imagem3);
+        this.listpostagens.add(postagem);
+
+        postagem = new Postagem("Eliene dos Santos", "#Bahia Salvador", R.drawable.imagem4);
+        this.listpostagens.add(postagem);
     }
 }
--- CardView/app/src/main/java/br/com/mcode/cardview/activity/adapter/AdapterPostagem.java
+++ CardView/app/src/main/java/br/com/mcode/cardview/activity/adapter/AdapterPostagem.java
@@ -2,37 +2,58 @@ package br.com.mcode.cardview.activity.adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.List;
+
+import br.com.mcode.cardview.R;
+import br.com.mcode.cardview.model.Postagem;
+
 public class AdapterPostagem extends RecyclerView.Adapter<AdapterPostagem.MyViewHolder>{
+
+    private List<Postagem> postagemList;
+
+    public AdapterPostagem(List<Postagem> list){
+        this.postagemList = list;
+    }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View itemLista = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.postagem_detalhe, viewGroup, false);
+        return new MyViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHlder, int i) {
+        Postagem postagem = postagemList.get(i);
+        myViewHlder.textNome.setText(postagem.getNome());
+        myViewHlder.textPostagem.setText(postagem.getPostagem());
+        myViewHlder.imagemPostagem.setImageResource(postagem.getImagem());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return postagemList.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
         //Atributos
-        private TextView nome;
-        private TextView postagem;
-        private ImageView imagem;
+        private TextView textNome;
+        private TextView textPostagem;
+        private ImageView imagemPostagem;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            textNome = itemView.findViewById(R.id.textNome);
+            textPostagem = itemView.findViewById(R.id.textPostagem);
+            imagemPostagem = itemView.findViewById(R.id.imagePostagem);
         }
     }
 }
