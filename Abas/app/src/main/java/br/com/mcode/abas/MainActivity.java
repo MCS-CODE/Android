3bc18180c3b801955553dc8eb3444777f33a34aa
mcsdevbr <m.c.s@msn.com>
2022-03-09 21:36:19 -0300

    Abas

---
+++ Abas/app/src/main/java/br/com/mcode/abas/MainActivity.java
@@ -0,0 +1,44 @@
+package br.com.mcode.abas;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+import br.com.mcode.abas.fragment.EmAltaFragment;
+import br.com.mcode.abas.fragment.HomeFragment;
+import br.com.mcode.abas.fragment.InscricoesFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+        viewPager = findViewById(R.id.viewPager);
+
+        //Configurar um adapter
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add(R.string.tituloA, HomeFragment.class)
+                        .add(R.string.tituloB, InscricoesFragment.class)
+                        .add(R.string.tituloC, EmAltaFragment.class)
+                .create()
+        );
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+    }
+
+}
